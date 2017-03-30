9c7af6cd8d343a6230dd481d60f427cebaccecb3
shaikiffu <shaikiffu@gmail.com>
2017-03-30 08:53:29 +0530

    Added Popularity to Django

--- movies/forms.py
+++ movies/forms.py
@@ -1,20 +1,26 @@
 from django import forms 
 from django.contrib.auth.models import User
 
-from .models import Picture
+from .models import Picture, Film
 
 class PictureForm(forms.ModelForm):
 
     class Meta:
         model = Picture
-        fields = ['picture_id', 'picture_title']
+        fields = ['picture_id', 'picture_title', 'genres', 'ratings', 'picture_logo']
 
+class FilmForm(forms.ModelForm):
+
+    class Meta:
+        model = Film
+        fields = ['film_id', 'film_title', 'pop_genres', 'pop_ratings', 'pop_picture_logo']
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username','email','password'] 
+        fields = ['username','email','password']
+
 
 
--- movies/models.py
+++ movies/models.py
@@ -2,13 +2,25 @@ from django.contrib.auth.models import Permission, User
 from django.db import models
 from decimal import Decimal
 
-class Picture(models.Model):
-	user = models.ForeignKey(User, default=1)
-	picture_id = models.IntegerField(default=0)
-	picture_title = models.CharField(max_length=500)
-	genres = models.CharField(max_length=1000)
-	ratings = models.DecimalField(max_digits=10,decimal_places=1)
-	picture_logo = models.ImageField()
 
-	def __str__(self):
-        	return self.picture_title
+class Picture(models.Model):
+    user = models.ForeignKey(User, default=1)
+    picture_id = models.IntegerField(default=0)
+    picture_title = models.CharField(max_length=500)
+    genres = models.CharField(max_length=1000)
+    ratings = models.DecimalField(max_digits=10, decimal_places=1)
+    picture_logo = models.CharField(max_length=1000, default='/media/noimage.jpg')
+
+    def __str__(self):
+        return self.picture_title
+
+
+class Film(models.Model):
+    film_id = models.IntegerField(default=0)
+    film_title = models.CharField(max_length=500)
+    pop_genres = models.CharField(max_length=1000)
+    pop_ratings = models.DecimalField(max_digits=10, decimal_places=1)
+    pop_picture_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.film_title
